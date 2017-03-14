e8ea69fa8d97796d84ed9848e3599739a521df93
musikele <michele.nasti@gmail.com>
2017-03-14 10:50:17 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+// @flow
+
+const express = require('express');
+
+var app = express();
+
+app.get('/', (req, res) => {
+	//res.send('<h1>Hello Express!</h1>');
+	res.send({
+		name: 'Michele',
+		likes: [
+			'Biking',
+			'Cities'
+		]
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.send('About page');
+});
+
+app.get('/bad', (req, res) => {
+	res.send({
+		errorMessage: 'This route is bad'
+	});
+});
+
+app.listen(3000);
+
